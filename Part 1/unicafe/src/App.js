6f4d06508350319121644a537ebe8ae54329dda7
AlbertoBallesterosRivas <ballesterosrivasa@gmail.com>
2022-07-17 18:53:46 +0200

    Part 1 exercise 1.8

--- Part 1/unicafe/src/App.js
+++ Part 1/unicafe/src/App.js
@@ -1,22 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
-  }
+  };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
-  }
+  };
 
   const handleBadClick = () => {
     setBad(bad + 1);
-  }
+  };
 
   return (
     <div>
@@ -24,15 +24,23 @@ const App = () => {
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {good + neutral + bad}</p>
       <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good * 100 / (good + neutral + bad) } %</p>
-    </div>
-  )
-}
+      <p>positive {(good * 100) / (good + neutral + bad)} %</p>
+    </>
+  );
+};
 
-export default App
+export default App;
